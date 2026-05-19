d0f3dcc9c0ac2b00cd9c3ef42ec628c68ba4e3a0
Aleksandr <alpatovaleksandr342@gmail.com>
2026-05-19 22:31:37 +0300

    Сделал страницу поставщиков

--- packages/client/src/pages/SuppliersPage/SupplierPage.tsx
+++ packages/client/src/pages/SuppliersPage/SupplierPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { SupplierList } from './SupplierList';
+import { Button } from "@mantine/core";
+import { SupplierModal } from './SupplierModal';
+import { useDisclosure } from '@mantine/hooks';
+import type { updateSupplierInput } from 'shared';
 
 export const SupplierPage = () => {
+  const [opened, { open, close }] = useDisclosure();
+  const [supplier, setSupplier] = useState<updateSupplierInput | null>(null);
+  const onCreate = () => {setSupplier(null); open()}
+  const onUpdate = (value: updateSupplierInput) => {setSupplier(value); open()}
   return (
-    <div></div>
+    <>
+    <Button onClick={onCreate}>Добавить</Button>
+    <SupplierList open = {onUpdate}/>
+
+    {opened && <SupplierModal opened={opened} close={close} supplier={supplier}/>}
+    </>
+
+    
   )
 }
--- packages/server/src/routers/index.ts
+++ packages/server/src/routers/index.ts
@@ -2,10 +2,12 @@ import { router } from '../trpc';
 import { authRouter } from './auth';
 import { categotiesRouter } from './categories';
 import { ProductRouter } from './products';
+import { supplierRouter } from './supplier';
 export const appRouter = router({
   auth: authRouter,
   categoties: categotiesRouter,
-  products: ProductRouter
+  products: ProductRouter,
+  suppliers: supplierRouter
 });
 
 export type AppRouter = typeof appRouter;
